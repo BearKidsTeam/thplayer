a8d653e7924d664998de3c25f2b4f377ebe01ad3
BLumia <wzc782970009@gmail.com>
2017-10-18 14:03:08 +0800

    still wip

--- site.js
+++ site.js
@@ -1,19 +1,35 @@
 const app1 = new Moon({
 	el: "#app1",
 	data: {
-		msg: "Hello Moon!",
-		osname: "windows",
-		msg_map: {
-			windows: "Windows supported!",
-			mac: "osx is a bug",
-			linux: "Linux fully powered!"
-		}
+		osname: "windows"
 	},
 	methods: {
 		setos: function(os) {
 			this.set('osname', os);
-			this.set('msg', this.get('msg_map')[os]);
 		}
 	}
 });
-app1.set('msg', "Changed Message!");
+// osname can be: windows, mac, linux
+function getOS() {
+	var userAgent = window.navigator.userAgent,
+		platform = window.navigator.platform,
+		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+//		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+		os = 'linux'; // default
+
+	if (macosPlatforms.indexOf(platform) !== -1) {
+		os = 'mac';
+//	} else if (iosPlatforms.indexOf(platform) !== -1) {
+//		os = 'iOS';
+	} else if (windowsPlatforms.indexOf(platform) !== -1) {
+		os = 'windows';
+//	} else if (/Android/.test(userAgent)) {
+//		os = 'Android';
+	} else if (!os && /Linux/.test(platform)) {
+		os = 'linux';
+	}
+
+	return os;
+}
+app1.set('osname', getOS());
